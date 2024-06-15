f83d5852244dc445a64f7730037638c138f10629
Andrew Athalye <andrewathalye@outlook.com>
2024-06-15 01:00:31 +0100

    Fix gnatstudio

--- default.nix
+++ default.nix
@@ -1,12 +1,13 @@
-{ system ? builtins.currentSystem
-, adapkgs-internal ? import <nixpkgs> {inherit system; overlays =
-   [
-    (final: prev: { gnat = prev.gnat13; })
-    (final: prev: { gnat12 = prev.gnat13; })
-   ];}
-}:
+{ system ? builtins.currentSystem }:
 
 # We require GNAT 13 to match the current system compiler GCC 13
+let
+   adapkgs-internal = import <nixpkgs> {inherit system; overlays =
+      [
+         (final: prev: { gnat = prev.gnat13; })
+         (final: prev: { gnat12 = prev.gnat13; })
+      ];};
+in
 with adapkgs-internal;
 with python3Packages;
 rec {
@@ -27,14 +28,15 @@ rec {
    wayland-ada = callPackage ./wayland-ada {wayland-ada-scanner = wayland-ada-scanner; alire-index = alire-index;};
 
    # Updated GNATCOLL for Python <=3.12 support
-   gnatcoll-python3-patched = gnatcoll-python3.overrideAttrs (final:
+   gnatcoll-python3-patched-p1 = gnatcoll-python3.overrideAttrs (final:
     {
       version = "24.2";
       src = fetchzip {
          url = "https://github.com/AdaCore/gnatcoll-bindings/archive/refs/heads/24.2.zip";
          hash = "sha256-LoeZspeO5siSuIcA6iuRABlpfSlpJJuxnhSvlbIYfzE=";
       };
-    }).override {python3 = python3;};
+    });
+   gnatcoll-python3-patched = gnatcoll-python3-patched-p1.override {python3 = python3;};
    # Force this gnatcoll to use the current default Python version (instead of hardcoded 3.9)
 
    types-gdb = callPackage ./types-gdb {};  
@@ -66,5 +68,5 @@ rec {
    ada-language-server-glib = ada-language-server.override { glibSupport = true; };
 
    # Tier E
-   gnatstudio = callPackage ./gnatstudio { gtkada = gtkada; libadalang = libadalang; libadalang-python = libadalang-python; libadalang-tools = libadalang-tools; vss = vss; ada-spawn-glib = ada-spawn-glib; ada-language-server-glib = ada-language-server-glib; ada-language-server = ada-language-server; gnatcoll-python3-patched = gnatcoll-python3-patched;};
+   gnatstudio = callPackage ./gnatstudio { gtkada = gtkada; libadalang = libadalang; libadalang-python = libadalang-python; libadalang-tools = libadalang-tools; vss = vss; ada-spawn-glib = ada-spawn-glib; ada-language-server-glib = ada-language-server-glib; ada-language-server = ada-language-server; gnatcoll-python3-patched = gnatcoll-python3-patched; };
 }
