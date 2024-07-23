a840f09eef886624a3549713ea194ef6fc90df8f
Andrew Athalye <andrewathalye@outlook.com>
2024-07-23 16:45:38 -0400

    Tentatively fix d_bus-ada bug

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@
 
 nix-ada.pkgs.mkShell {
 #   nativeBuildInputs = [ nix-ada.gnatstudio nix-ada.polyorb ];
-#   buildInputs = [ nix-ada.florist nix-ada.wayland-ada nix-ada.polyorb nix-ada.dbus-ada ];
+   buildInputs = [ nix-ada.dbus-ada ];
 }
